f4dfcae3caf55a1937e47eabbfb51d9aadaf9ca9
Štěpán Venclík <stepan.venclik@gmail.com>
2022-11-11 21:32:46 +0100

    fix root_path generation

--- cmd_quiz.py
+++ cmd_quiz.py
@@ -1,7 +1,11 @@
 import src.cmd_quiz as q
-import pathlib
+import os
+import sys
 
-ROOT_PATH = pathlib.Path(__file__).parent
+if getattr(sys, 'frozen', False):
+    application_path = os.path.dirname(sys.executable)
+else:
+    application_path = os.path.dirname(__file__)
 
 if __name__ == '__main__':
-    q.run_cli_app(ROOT_PATH)
+    q.run_cli_app(application_path)
